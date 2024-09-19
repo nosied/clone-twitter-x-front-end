9feed81127054270d24c5ad2d2eda05e1dc78d4d
nosied <deisonribeiro14@gmail.com>
2024-09-19 00:32:39 -0300

    feat: initial settings for project

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Twitter",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/auth/signin-form.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Input } from "../ui/input";
+
+export const SigninForm = () => {
+  const router = useRouter();
+  const [emailField, setEmailField] = useState("");
+  const [passwordField, setPasswordField] = useState("");
+
+  const handleEnterButton = () => {
+    router.replace("/home");
+  };
+
+  return (
+    <>
+      <Input
+        placeholder="Digite seu e-mail"
+        value={emailField}
+        onChange={(t) => setEmailField(t)}
+      />
+
+      <Input
+        placeholder="Digite sua senha"
+        value={passwordField}
+        onChange={(t) => setPasswordField(t)}
+        password
+      />
+      <button onClick={handleEnterButton}>Entrar</button>
+    </>
+  );
+};
