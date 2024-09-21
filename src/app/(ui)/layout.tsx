fcbd2b215db60e326c307eddc54900acd8027b1b
Deison Ribeiro <deisonribeiro14@gmail.com>
2024-09-20 23:11:32 -0300

    feat: finished left-lateral-menu

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -1,4 +1,6 @@
 import { NavItem } from "@/components/nav/nav-item";
+import { NavLogout } from "@/components/nav/nav-logout";
+import { NavMyProfile } from "@/components/nav/nav-myprofile";
 import { Logo } from "@/components/ui/logo";
 import { faHouse, faUser } from "@fortawesome/free-solid-svg-icons";
 import { ReactNode } from "react";
@@ -17,7 +19,10 @@ export default function Layout({ children }: Props) {
             <NavItem href="/profile" icon={faUser} label="Meu Perfil" />
           </nav>
         </div>
-        <div>..</div>
+        <div className="mb-6 flex flex-col gap-4">
+          <NavLogout />
+          <NavMyProfile />
+        </div>
       </section>
       <section className="flex-1 max-w-lg">{children}</section>
       <aside className="hidden lg:flex flex-col gap-6 sticky top-0 h-fit w-96 px-8 py-6 border-l-2 border-gray-900">
---
+++ src/data/user.ts
@@ -0,0 +1,12 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+  slug: "deisson",
+  name: "Deisson",
+  avatar: "https://ui-avatars.com/api/?background=random&name=Deisson&size=100",
+  cover:
+    "https://img.freepik.com/fotos-gratis/paisagem-de-nevoeiro-matinal-e-montanhas-com-baloes-de-ar-quente-ao-nascer-do-sol_335224-794.jpg",
+  bio: "Bio do Deisson",
+  link: "https://github.com/deisson",
+  postCount: 123,
+};
