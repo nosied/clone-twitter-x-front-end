9e45e6a0a56f670e3e2fcea0b2347f5091ae4d75
Deison Ribeiro <deisonribeiro14@gmail.com>
2024-09-23 01:09:23 -0300

    feat: created search results page

---
+++ src/app/(ui)/search/page.tsx
@@ -0,0 +1,27 @@
+import { TweetItem } from "@/components/tweet/tweet-item";
+import { GeneralHeader } from "@/components/ui/general-header";
+import { SearchInput } from "@/components/ui/search-input";
+import { tweet } from "@/data/tweet";
+import { redirect } from "next/navigation";
+
+type Props = {
+  searchParams: {
+    q: string | undefined;
+  };
+};
+export default function Page({ searchParams }: Props) {
+  if (!searchParams.q) redirect("/");
+  return (
+    <div className="">
+      <GeneralHeader backHref="/">
+        <SearchInput defaultValue={searchParams.q} />
+      </GeneralHeader>
+      <div className="border-t-2 borrder-gray-900">
+        <TweetItem tweet={tweet} />
+        <TweetItem tweet={tweet} />
+        <TweetItem tweet={tweet} />
+        <TweetItem tweet={tweet} />
+      </div>
+    </div>
+  );
+}
